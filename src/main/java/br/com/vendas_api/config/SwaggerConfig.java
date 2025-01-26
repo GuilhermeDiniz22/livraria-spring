238e2345599ccb084796a6a6b921a93e340fb844
Guilherme22 <gdrg@discente.ifpe.edu.br>
2025-01-26 19:04:02 -0300

    adicionado bearer token no swagger

--- src/main/java/br/com/vendas_api/config/SwaggerConfig.java
+++ src/main/java/br/com/vendas_api/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package br.com.vendas_api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,7 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("livraria-spring")
@@ -19,7 +23,14 @@ public class SwaggerConfig {
                         .description("API de uma livraria online")
                         .contact(new Contact()
                                 .name("Guilherme Diniz Java DEV")
-                                .email("guilhermediniz303@gmail.com")));
+                                .email("guilhermediniz303@gmail.com")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 
     @Bean
