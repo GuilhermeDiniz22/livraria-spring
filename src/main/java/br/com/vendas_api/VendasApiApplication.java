8253f7370416ddcfc3d5f3058016860e859cae46
Guilherme Diniz dos Reis Gomes <gdrg@discente.ifpe.edu.br>
2025-02-19 12:27:45 -0300

    add railway

--- src/main/java/br/com/vendas_api/VendasApiApplication.java
+++ src/main/java/br/com/vendas_api/VendasApiApplication.java
@@ -2,11 +2,13 @@ package br.com.vendas_api;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Livraria API", version = "2.1", description = "Uma api para ser usada em uma livraria"))
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Uma API para ser usada em uma livraria") })
 public class VendasApiApplication {
 
 	public static void main(String[] args) {
